457342e2c27c347d1db32d9a2dbafe72f2c3c457
mrcn4 <koczo123@gmail.com>
2014-06-05 13:18:12 +0200

    Test morfologic interactive

--- src/pl/wedt/bigrams/MainClass.java
+++ src/pl/wedt/bigrams/MainClass.java
@@ -1,9 +1,44 @@
 package pl.wedt.bigrams;
 
-public class MainClass {
+import java.io.ObjectInputStream.GetField;
+import java.util.List;
+import java.util.Scanner;
 
+import morfologik.stemming.WordData;
+
+public class MainClass {
+	static morfologik.stemming.PolishStemmer dict = new morfologik.stemming.PolishStemmer();
+	
+	private static String getStem(String word)
+	{
+		List<WordData> lookup = dict.lookup(word.toLowerCase());
+		if (lookup.size() > 0)
+		{
+			//return first
+			return lookup.get(0).getStem().toString();
+		}
+		else
+		{
+			return "";
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Bigrams");
+		String end = "q";
+		String input = "";
+		
+		System.out.println("Bigrams morfologic test");
+		System.out.println("Enter q to quit, enter word to see it's stem");
+		
+		Scanner sc = new Scanner(System.in);
+		do{
+			input = sc.next();
+			System.out.println("Stem for \"" + input + "\" is " + getStem(input));
+		}
+		while(!input.equals(end));
+		
+		
+		
 	}
 
 }
